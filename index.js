1f25db6495d011f16e0455d86a15e2b9a7740d7c
Jordan Davis <jordandavis4799@gmail.com>
2023-07-27 03:04:09 -0400

    added import

--- index.js
+++ index.js
@@ -5,24 +5,14 @@ const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
 const urlencodedParser = bodyParser.urlencoded({ extended: true });
 const cors = require("cors");
+const HttpError = require("./models/http-error");
 
 const deckRoutes = require("./routes/deck-routes");
 const userRoutes = require("./routes/user-routes");
-// const gameRoutes = require("./routes/game-routes");
-
-const LeaderboardStat = require("./models/leaderboard");
 
 app.use(jsonParser);
 app.use(urlencodedParser);
 app.use(cors());
-
-// app.use((req, res, next) => {
-// 	res.setHeader("Access-Control-Allow-Origin", "*");
-// 	res.setHeader("Access-Control-Allow-Credentials", "true");
-// 	res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE");
-// 	res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-// 	next();
-// });
 
 app.use((req, res, next) => {
 	console.log("set headers log");
